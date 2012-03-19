8b314d8fde5e407641e5ec475897a3294c4764c2
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-03-20 01:57:46 +0300

    Добавление пустого приложения.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtsapp::Application.config.secret_token = '5e452b7a2ab275cb78d789646eb20de68f20de3a88809bcd1f917b5b6801dc7c63dc2d7ec8134e121d3f8d7cfd3dba92aac49d261216888567a48931b6276535'
