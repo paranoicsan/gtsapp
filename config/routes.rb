bb262b16c83741d5f6b5d5630b6e0c8d41b283e0
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-03-21 01:06:10 +0300

    505251-26613223: Пройден первый тест.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Gtsapp::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
+  root :to => "home#index"
+
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,4 +1,10 @@
 # encoding: utf-8
-Given /^Я на странице "([^"]*)"$/ do |arg|
+Given /^Я на главной странице$/ do
+  visit "/"
+end
+When /^Я заполняю "([^"]*)" значением "([^"]*)"$/ do |arg1, arg2|
+  pending
+end
+Then /^Я должен увидеть "([^"]*)"$/ do |arg|
   pending
 end
