406ce910693a634fd437e0120e68b69784a496c2
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-03-21 18:45:47 +0300

    Срабатывают еще 2 шага из сценария по входу в систему.

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -3,8 +3,8 @@ Given /^Я на главной странице$/ do
   visit "/"
 end
 When /^Я заполняю "([^"]*)" значением "([^"]*)"$/ do |arg1, arg2|
-  pending
+  fill_in arg1, :with => arg2
 end
 Then /^Я должен увидеть "([^"]*)"$/ do |arg|
-  pending
+  assert page.has_content?(arg)
 end
