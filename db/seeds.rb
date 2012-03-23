19f4ea7553a6e8e5754258c2c91f959dfc319ca9
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-03-23 13:03:55 +0300

    Добавлен постоянный тестовый пользователь.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: 't_username', password: '1111', password_confirmation: '1111', email: 'temail@test.com').save!
