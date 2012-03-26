6406d41b3545885f3d33452c34dc33b1d73b6452
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-03-27 01:08:10 +0300

    Вспомогательные методы вынесены из тела контроллера.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -65,19 +65,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # Сохраняет в сессии путь, куда хотят попасть
-  def store_destination
-    session[:return_to] = request.fullpath
-  end
 
-  # Сохраняет в сессии путь, откуда пришли
-  def store_referrer
-    session[:return_to] = request.referer
-  end
-
-  def redirect_back_or_default(default)
-    redirect_to(session[:return_to] || default)
-    session[:return_to] = nil
-  end
 
 end
