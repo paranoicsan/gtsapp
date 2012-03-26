f9853add7f9f7afeff25c1a3127e49e6f5e2844b
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-03-27 01:30:03 +0300

    Исправления в вспомогательном модуле ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -65,6 +65,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
 
 end
