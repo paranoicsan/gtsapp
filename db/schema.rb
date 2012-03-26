4b0df17f60d045c1bf2338a746284dfec75641a3
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-03-26 23:39:55 +0300

    [#27005001] Включены роли доступа гемом easy roles. Добавлена одна миграция.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120322064553) do
+ActiveRecord::Schema.define(:version => 20120326202517) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20120322064553) do
     t.string   "persistence_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "roles",             :default => "--- []"
   end
 
 end
