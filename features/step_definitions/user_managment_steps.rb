3febe6dd6c8381d324ccc350467cd0a3e960ce6a
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-03-30 00:04:49 +0300

    [#26613229] Никто, кроме администраторо не видит сылки на список пользователей.

--- features/step_definitions/user_managment_steps.rb
+++ features/step_definitions/user_managment_steps.rb
@@ -13,3 +13,13 @@ Then /^Я вижу список пользователей$/ do
   #save_and_open_page
   page.find("h1.section-title").should have_content "Пользователи"
 end
+Given /^Я - не администратор системы$/ do
+  @user = create_user
+  @user.save_without_session_maintenance
+  #noinspection RubyResolve
+  @user.add_role "admin"
+  @user.save
+  #noinspection RubyResolve
+  visit login_path
+  login @user
+end
