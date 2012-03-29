72e95a9448ff3a9475b17a6ab35c85703220140e
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-03-30 00:04:49 +0300

    [#26613229] Никто, кроме администраторо не видит сылки на список пользователей.

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -2,9 +2,16 @@
 include ApplicationHelper
 
 When /^Я нажимаю на ссылку "([^"]*)"$/ do |link_text|
-  #save_and_open_page
   click_link link_text
 end
 When /^Я нахожусь на странице "([^"]*)"$/ do |title|
   page.find('h1.section-title').should have_content title
+end
+Then /^Я не вижу ссылки "([^"]*)"$/ do |title|
+  #save_and_open_page
+  page.all('a').each do |link|
+    if link.text.eql?(title)
+      assert false, 'Ссылка видна на странице.'
+    end
+  end
 end
