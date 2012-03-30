696e2e7775960615b47fdc1d16e6a4ab337105c7
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-03-30 08:59:46 +0300

    Настройки spork для работы с simpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,11 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  require 'simplecov'
-  SimpleCov.start 'rails'
+  unless ENV['DRB']
+    require 'simplecov'
+    SimpleCov.start 'rails'
+  end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
@@ -17,6 +20,7 @@ Spork.prefork do
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
+#noinspection RubyResolve
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   RSpec.configure do |config|
@@ -29,24 +33,27 @@ Spork.prefork do
     # config.mock_with :rr
 
     # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    #config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
     # instead of true.
-    config.use_transactional_fixtures = true
+    #config.use_transactional_fixtures = true
 
     # If true, the base class of anonymous controllers will be inferred
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
+    #noinspection RubyResolve
     config.infer_base_class_for_anonymous_controllers = false
   end
 end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
-  require 'simplecov'
-  SimpleCov.start 'rails'
+  if ENV['DRB']
+    require 'simplecov'
+    SimpleCov.start 'rails'
+  end
 end
 
 # --- Instructions ---
