0df449a35d8f292af509f2e6c675c000f7808372
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-07-16 14:59:35 +0300

    [#30947801] Добавлена метод-ссылка для получения бонусного продукта.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+  ##
+  # Вовзращает объект бонусного продукта
+  # @return Product
+  def bonus_product
+    Product.find(self.bonus_product_id)
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,12 @@
+#encoding: utf-8
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "Должен возвращать объект бонусного продукта по ссылке bonus_product" do
+    p1 = Product.create!({:name => 'prod_1'})
+    p2 = Product.create!({:name => 'prod_2', :bonus_product_id => p1.id})
+    assert p2.bonus_product.name == p1.name, 'Не работает ссылка на бонусный продукт'
+  end
+
 end
