75c8827c229f8a208e211d97573b2a00f9d473ca
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-08-10 13:03:17 +0300

    Добавлены отдельные scope для выборки определённых рубрик.

--- app/models/rubric.rb
+++ app/models/rubric.rb
@@ -3,17 +3,19 @@ class Rubric < ActiveRecord::Base
   has_many :keywords
   belongs_to :rubric_keyword
   validates_presence_of :name
+  scope :social, where(:social => true)
+  scope :commercial, where(:social => false)
 
   def self.by_rubricator(rubricator)
     case rubricator
       # Социальный
       when 1
-        Rubric.find_all_by_social true
+        Rubric.social.order('name')
       # коммерческий
       when 2
-        Rubric.find_all_by_social false
+        Rubric.commercial.order('name')
       else
-        Rubric.all
+        Rubric.order('name').all
     end
   end
 
