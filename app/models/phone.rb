e237d92cef5ef74b54036811820fbfed44787ad0
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-09-17 13:45:01 +0300

    Каждый новый телефон создаётся с пониженным порядком #GTS-144 fixed

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,4 +1,21 @@
 class Phone < ActiveRecord::Base
   belongs_to :branch
   validates_presence_of :name
+  before_save :check_fields
+
+  ##
+  # Определяет самый низкий порядок отображения
+  # @return [Integer] Самый низкий порядок отображения
+  def next_num_order
+    # текущий последний телефон по отображению
+    cur_last_phone = Phone.where("branch_id = ?", [branch_id]).order("order_num DESC").first
+    cur_last_phone ? cur_last_phone.order_num + 1 : 1
+  end
+
+  private
+  def check_fields
+    if order_num.nil?
+      self.order_num = next_num_order
+    end
+  end
 end
--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -12,4 +12,18 @@ describe Phone do
     phone.should have(1).error_on(:name)
   end
 
+  describe "обработка порядка отображения" do
+
+    it "первый телефон всегд имеет порядок отображения равный 1" do
+      phone = FactoryGirl.create :phone
+      phone.order_num.should eq(1)
+    end
+    it "каждый следующий телефон создаётся с повышенным индексом отображения" do
+      phone_origin = FactoryGirl.create :phone
+      phone = FactoryGirl.create :phone, branch_id: phone_origin.branch_id
+      phone.order_num.should eq(2)
+    end
+
+  end
+
 end
