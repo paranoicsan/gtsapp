80942a2c3420c70347555e1de3b8362b36782f34
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-10-17 11:03:10 +0300

    Изменения в отображении компании и в отображениях списков разных объектов.

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,2 +1,24 @@
+#Encoding: utf-8
 module CompaniesHelper
+  def status_span(status)
+    case status
+      when CompanyStatus.active
+        val = 'label-success'
+      when CompanyStatus.suspended
+        val = ''
+      when CompanyStatus.archived
+        val = ''
+      when CompanyStatus.queued_for_delete
+        val = 'label-important'
+      when CompanyStatus.need_attention
+        val = 'label-warning'
+      when CompanyStatus.need_improvement
+        val = 'label-info'
+      when CompanyStatus.second_suspend
+        val = ''
+      else
+        val = 'Неизвестный статус'
+    end
+    content_tag :span, status.name, class: "label #{val}"
+  end
 end
