e207040c1aed6783e4907dcbf753e61ccc84f61a
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-11-26 15:10:54 +0300

    Попытка удалённо запустить тесты.

--- features/support/env.rb
+++ features/support/env.rb
@@ -49,22 +49,22 @@ Spork.prefork do
   Capybara.javascript_driver = :selenium_extended_http_timeout
 
 
-  #if ENV['HEADLESS']
-  #
-  #  require 'headless'
-  #  headless = Headless.new
-  #  at_exit do
-  #    headless.destroy
-  #  end
-  #
-  #  Before("@selenium,@javascript", "~@no-headless") do
-  #    headless.start if Capybara.current_driver == :selenium
-  #  end
-  #
-  #  After("@selenium,@javascript", "~@no-headless") do
-  #    headless.stop if Capybara.current_driver == :selenium
-  #  end
-  #end
+  if ENV['HEADLESS']
+
+    require 'headless'
+    headless = Headless.new
+    at_exit do
+      headless.destroy
+    end
+
+    Before("@selenium,@javascript", "~@no-headless") do
+      headless.start if Capybara.current_driver == :selenium
+    end
+
+    After("@selenium,@javascript", "~@no-headless") do
+      headless.stop if Capybara.current_driver == :selenium
+    end
+  end
 
 end
 
