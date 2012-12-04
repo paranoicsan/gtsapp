07a228507e8050a81a29db53a332b88c1fa6d9d0
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-12-04 18:41:07 +0300

    GTS-208 Отчёт должен формироваться с указанием числа месяца

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -54,9 +54,8 @@ class ReportController < ApplicationController
     agent_id = params[:report_agent]
     @report_agent = User.find agent_id
 
-    start = "1-#{params[:report_period_start][:month]}-#{params[:report_period_start][:year]}"
-    end_p = Date.civil params[:report_period_end][:year].to_i, params[:report_period_end][:month].to_i, -1
-    #end_p = "#{params[:report_period_end][:day]}-#{params[:report_period_end][:month]}-#{params[:report_period_end][:year]}"
+    start = Date.civil params[:report_period_start][:year].to_i, params[:report_period_start][:month].to_i, params[:report_period_start][:day].to_i
+    end_p = Date.civil params[:report_period_end][:year].to_i, params[:report_period_end][:month].to_i, params[:report_period_end][:day].to_i + 1
 
     @report_result = CompanyHistory.where("user_id = ? and created_at >= ? AND created_at <= ? ", agent_id, start, end_p)
     render :layout => false
