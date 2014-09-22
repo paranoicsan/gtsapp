ac5837d404ce3c4ceba707a494c29985ac985433
Alexander Kuznetsov <paranoic.san@gmail.com>
2014-09-22 09:18:07 +0300

    Fixes migration for clearing Branches & Websites

--- db/migrate/20140921131734_clear_branch_website.rb
+++ db/migrate/20140921131734_clear_branch_website.rb
@@ -1,15 +1,7 @@
 class ClearBranchWebsite < ActiveRecord::Migration
   def change
-    rename_table :branch_websites, :branches_websites
-  end
-
-  def up
     remove_column :branch_websites, :id
     remove_column :branch_websites, :old_branch_id
-  end
-
-  def down
-    add_column :branch_websites, :id, :integer
-    add_column :branch_websites, :old_branch_id, :integer
+    rename_table :branch_websites, :branches_websites
   end
 end
